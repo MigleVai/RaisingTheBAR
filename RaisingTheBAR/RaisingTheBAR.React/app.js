0e85c6c49ffc475a12fb6a4f64901ef2fd973299
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-03-20 19:54:47 +0200

    Made the react web runable from vs2017

---
+++ RaisingTheBAR/RaisingTheBAR.React/app.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const opn = require('opn');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/hello', (req, res) => {
+    res.send({ express: 'Hello From Express' });
+});
+
+app.listen(port, () => {
+    ls();
+    console.log(`Listening on port ${port}`);
+});
+async function ls() {
+    const { stdout, stderr } = await exec('yarn client');
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+}
