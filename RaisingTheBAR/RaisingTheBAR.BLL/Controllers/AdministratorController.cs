0c253f3311215d3599f33859c2edc628b8eaf0f2
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-03-22 17:34:53 +0200

    BE: Added User blocking for Administrator

---
+++ RaisingTheBAR/RaisingTheBAR.BLL/Controllers/AdministratorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RaisingTheBAR.BLL.Models.RequestModels;
+using RaisingTheBAR.Core.Models;
+
+namespace RaisingTheBAR.BLL.Controllers
+{
+    [Authorize("Administrator")]
+    [Produces("application/json")]
+    [Route("api/Administrator")]
+    public class AdministratorController : Controller
+    {
+        private readonly DbContext _dbContext;
+        public AdministratorController(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpPost]
+        public IActionResult ChangeBlock([FromBody]BlockRequest request)
+        {
+            var userContext = _dbContext.Set<User>();
+            var user = userContext.FirstOrDefault(x => x.Email == request.Email);
+
+            if (user == null)
+            {
+                return BadRequest($"User with email: {request.Email} not found");
+            }
+
+            if (user.Blocked == request.Blocked)
+            {
+                var str = user.Blocked ? "already Blocked" : "not Blocked";
+                return BadRequest($"This user is { str }");
+            }
+
+            user.Blocked = request.Blocked;
+
+            var result = _dbContext.SaveChanges();
+            if (result > 0)
+            {
+                return Ok();
+            }
+
+            return BadRequest("Something wrong in db");
+        }
+    }
+}
---
+++ RaisingTheBAR/RaisingTheBAR.BLL/Models/RequestModels/BlockRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RaisingTheBAR.BLL.Models.RequestModels
+{
+    public class BlockRequest
+    {
+        public string Email { get; set; }
+        public bool Blocked { get; set; }
+    }
+}
