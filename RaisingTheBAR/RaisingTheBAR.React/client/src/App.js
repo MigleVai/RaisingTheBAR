a8bf17c6996e642330924b48fe7e17627bc39343
Miglė Vaitulevičiūtė <migle.vaituleviciute@gmail.com>
2018-03-22 15:15:49 +0200

    Added carousel to App and Cleaned MenuBar in App

--- RaisingTheBAR/RaisingTheBAR.React/client/src/App.js
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/App.js
@@ -2,22 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MenuBar from './components/MenuBar';
-import { MuiThemeProvider } from 'material-ui/styles';
+import ImgCarousel from './components/ImgCarousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MuiThemeProvider>
-          <MenuBar/>
-        </MuiThemeProvider>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header> {/*className="App-header"*/}
+          <MenuBar />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ImgCarousel />
       </div>
     );
   }
