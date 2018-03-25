a37bcbca00c438fee3df782b6408aeea266371e6
tautris <tautrimas.g2@gmail.com>
2018-03-25 23:56:28 +0300

    FE: Added some routing and renamed things

--- RaisingTheBAR/RaisingTheBAR.React/client/src/App.js
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-import MenuBar from './components/MenuBar';
+
+import Header from './components/Header';
 import ImgCarousel from './components/ImgCarousel';
-import Login from './components/Login';
-// import Signup from './components/Signup';
+import SignIn from './components/SignIn';
+import Register from './components/Register';
 import ItemList from './components/ItemList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header> {/*className="App-header"*/}
-          <MenuBar />
-        </header>
-        {/* <ImgCarousel /> */}
-        {/* <Login /> */}
-        {/* <Signup /> */}
-        <ItemList/>
-      </div>
+      <Router>
+        <div className="App">
+          <header> {/*className="App-header"*/}
+            <Header />
+          </header>
+          {/* <ImgCarousel /> */}
+          {/* <SignIn /> */}
+          {/* <Register /> */}
+          <Route path="/signin/" component={SignIn}/>
+          <Route path="/register/" component={Register}/>  
+          <Route path="/allitems/" component={ItemList}/>
+        </div>
+      </Router>
     );
   }
 }
--- RaisingTheBAR/RaisingTheBAR.React/client/src/components/Header.jsx
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/components/Header.jsx
@@ -3,12 +3,13 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import { Link } from 'react-router-dom';
 
 import SearchBar from './SearchBar';
 
 import { MuiThemeProvider } from 'material-ui/styles';
 
-export default class DrawerSimpleExample extends React.Component {
+export default class Header extends React.Component {
 
   constructor(props) {
     super(props);
@@ -18,8 +19,6 @@ export default class DrawerSimpleExample extends React.Component {
   handleToggle = () => this.setState({ open: !this.state.open });
 
   handleClose = () => this.setState({ open: false });
-
-
 
   render() {
     const styles = {
@@ -39,18 +38,23 @@ export default class DrawerSimpleExample extends React.Component {
               titleStyle={styles.align}
               iconClassNameRight="muidocs-icon-navigation-expand-more"
               onLeftIconButtonClick={this.handleToggle}
-              //iconElementRight={}
+            //iconElementRight={}
             >
-            <Drawer
-              docked={false}
-              width={200}
-              open={this.state.open}
-              onRequestChange={(open) => this.setState({ open })}
-            >
-              <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
-              <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
-            </Drawer>
-            <SearchBar />
+              <Drawer
+                docked={false}
+                width={200}
+                open={this.state.open}
+                onRequestChange={(open) => this.setState({ open })}
+              >
+                <Link to={"/allitems/"}>
+                  <MenuItem onClick={this.handleClose}>All items</MenuItem>
+                </Link>
+                <MenuItem onClick={this.handleClose}>Work in progress...</MenuItem>
+              </Drawer>
+              <SearchBar />
+              <Link to={"/signin/"}>  
+                <FlatButton  label="Sign in" backgroundColor="Red" hoverColor="Green"/>
+              </Link>
             </AppBar>
           </div>
         </MuiThemeProvider>
@@ -58,4 +62,3 @@ export default class DrawerSimpleExample extends React.Component {
     );
   }
 }
-
