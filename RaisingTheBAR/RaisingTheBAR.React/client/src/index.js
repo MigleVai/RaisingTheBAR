076c6b70b17771aa795d7af1aa389e1b6bace8f9
tautris <tautrimas.g2@gmail.com>
2018-03-29 18:32:27 +0300

    FE: Wrapped everything with muitheme and router

--- RaisingTheBAR/RaisingTheBAR.React/client/src/index.js
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/index.js
@@ -6,6 +6,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Router>, document.getElementById('root'));
+    
 registerServiceWorker();
