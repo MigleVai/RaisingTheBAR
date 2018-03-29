c48eaec94c66e2447bca1c1578bb2f62a90db3d6
Unknown <vytautaszilinas@gmail.com>
2018-03-29 19:34:10 +0300

    BE: FE: Fixed package-lock and vstudio project

--- RaisingTheBAR/RaisingTheBAR.React/client/src/index.js
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/index.js
@@ -7,6 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { MuiThemeProvider } from 'material-ui'
 
 ReactDOM.render(
     <Router>
