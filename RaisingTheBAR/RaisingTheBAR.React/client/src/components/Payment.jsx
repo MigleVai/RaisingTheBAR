e5d249357599ed32b61ddb6c889903b6907b72fb
IgnasBr <ignas.bradauskas@gmail.com>
2018-04-16 21:04:28 +0300

    FE: "Pay now" button now checks for empty fields and prints errors.

--- RaisingTheBAR/RaisingTheBAR.React/client/src/components/Payment.jsx
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/components/Payment.jsx
@@ -20,7 +20,8 @@ export default class Payment extends React.Component {
             number: '',
             cvvError: '',
             holderError: '',
-            numberError: ''
+            numberError: '',
+            response: ''
         };
         this.handleCvvChange = this.handleCvvChange.bind(this);
         this.handleExpMonthChange = this.handleExpMonthChange.bind(this);
@@ -36,9 +37,18 @@ export default class Payment extends React.Component {
     }
 
     // Need to check if the parameters are valid here.
-    handlePayClick() {
-        this.setState({ amount: 12 });
-        axios.post(`/api/Payment/ExecutePayment`,
+    handlePayClick(props) {
+        this.setState({ amount: 999999  });
+        var error = 'This field is required!';
+        if (this.state.cvv !== '' &&
+            this.state.holder !== '' &&
+            this.state.number !== '' &&
+            this.state.cvvError === '' &&
+            this.state.holderError === '' &&
+            this.state.numberError === '' &&
+            this.state.exp_month !== 0 &&
+            this.state.exp_year !== 0) {
+            axios.post(`/api/Payment/ExecutePayment`,
                 {
                     cvv: this.state.cvv,
                     exp_month: this.state.exp_month,
@@ -46,15 +56,27 @@ export default class Payment extends React.Component {
                     holder: this.state.holder,
                     number: this.state.number
                 })
-            .then(res => {
-                const result = res.data;
-                localStorage.setItem('jwtToken', result.token);
-                axios.defaults.headers.common['Authorization'] = 'Bearer ' + result.token;
-                this.props.onLogging(true);
-            })
-            .catch(function(error) {
+                .then(res => {
+                    const result = res.data;
 
-            });
+                    this.state.response = res.data;
+
+                })
+                .catch(function (error) {
+
+                });
+
+        } else {
+            if (this.state.cvvError !== '' || this.state.cvv === '') {
+                this.setState({ cvvError: error });
+            }
+            if (this.state.holderError !== '' || this.state.holder === '') {
+                this.setState({ holderError: error });
+            }
+            if (this.state.numberError !== '' || this.state.number === '') {
+                this.setState({ numberError: error });
+            }
+        }
     }
 
     handleCvvChange(event) {
@@ -126,7 +148,10 @@ export default class Payment extends React.Component {
                 margin: 'auto',
                 paddingTop: '6%',
                 width: '40%'
-               
+
+            },
+            buttonStyle: {
+                backgroundColor: '#929292'
             }
 
 
@@ -134,7 +159,7 @@ export default class Payment extends React.Component {
         return (
             //  <Jumbotron>
             <div style={styles.displayStyles}>
-                <ErrorMessage responseError={this.state.responseError}/>
+                <ErrorMessage responseError={this.state.responseError} />
                 <div>
                     <h3 style={styles.textStyle}>Check out</h3>
                 </div>
@@ -145,8 +170,8 @@ export default class Payment extends React.Component {
                         floatingLabelText="Credit Card Holder"
                         floatingLabelFixed={true}
                         style={styles.textFieldSytle}
-                        errorText={this.state.holderError}/>
-                    <br/>
+                        errorText={this.state.holderError} />
+                    <br />
                     <TextField
                         value={this.state.number}
                         onChange={(event) => this.handleNumberChange(event)}
@@ -154,57 +179,57 @@ export default class Payment extends React.Component {
                         floatingLabelFixed={true}
                         style={styles.textFieldSytle}
                         errorText={this.state.numberError} />
-                    <br/>
+                    <br />
                     <TextField
                         value={this.state.cvv}
                         onChange={(event) => this.handleCvvChange(event)}
                         floatingLabelText="Cvv"
                         floatingLabelFixed={true}
                         style={styles.textFieldSytle}
-                        errorText={this.state.cvvError}/>
-                    <br/>
+                        errorText={this.state.cvvError} />
+                    <br />
 
                     <DropDownMenu style={styles.customWidth} value={this.state.exp_year} onChange={this
                         .handleExpYearChange}>
-                        <MenuItem value={0} primaryText="Expiration year"/>
-                        <MenuItem value={2018} primaryText="2018"/>
-                        <MenuItem value={2019} primaryText="2019"/>
-                        <MenuItem value={2020} primaryText="2020"/>
-                        <MenuItem value={2021} primaryText="2021"/>
-                        <MenuItem value={2022} primaryText="2022"/>
-                        <MenuItem value={2023} primaryText="2023"/>
-                        <MenuItem value={2024} primaryText="2024"/>
-                        <MenuItem value={2025} primaryText="2025"/>
-                        <MenuItem value={2026} primaryText="2026"/>
-                        <MenuItem value={2027} primaryText="2027"/>
-                        <MenuItem value={2028} primaryText="2028"/>
-                        <MenuItem value={2029} primaryText="2029"/>
+                        <MenuItem value={0} primaryText="Expiration year" />
+                        <MenuItem value={2018} primaryText="2018" />
+                        <MenuItem value={2019} primaryText="2019" />
+                        <MenuItem value={2020} primaryText="2020" />
+                        <MenuItem value={2021} primaryText="2021" />
+                        <MenuItem value={2022} primaryText="2022" />
+                        <MenuItem value={2023} primaryText="2023" />
+                        <MenuItem value={2024} primaryText="2024" />
+                        <MenuItem value={2025} primaryText="2025" />
+                        <MenuItem value={2026} primaryText="2026" />
+                        <MenuItem value={2027} primaryText="2027" />
+                        <MenuItem value={2028} primaryText="2028" />
+                        <MenuItem value={2029} primaryText="2029" />
                     </DropDownMenu>
 
                     <DropDownMenu style={styles.customWidth} value={this.state.exp_month} onChange={this
                         .handleExpMonthChange}>
-                        <MenuItem value={0} primaryText="Expiration month"/>
-                        <MenuItem value={1} primaryText="1"/>
-                        <MenuItem value={2} primaryText="2"/>
-                        <MenuItem value={3} primaryText="3"/>
-                        <MenuItem value={4} primaryText="4"/>
-                        <MenuItem value={5} primaryText="5"/>
-                        <MenuItem value={6} primaryText="6"/>
-                        <MenuItem value={7} primaryText="7"/>
-                        <MenuItem value={8} primaryText="8"/>
-                        <MenuItem value={9} primaryText="9"/>
-                        <MenuItem value={10} primaryText="10"/>
-                        <MenuItem value={11} primaryText="11"/>
-                        <MenuItem value={12} primaryText="12"/>
+                        <MenuItem value={0} primaryText="Expiration month" />
+                        <MenuItem value={1} primaryText="1" />
+                        <MenuItem value={2} primaryText="2" />
+                        <MenuItem value={3} primaryText="3" />
+                        <MenuItem value={4} primaryText="4" />
+                        <MenuItem value={5} primaryText="5" />
+                        <MenuItem value={6} primaryText="6" />
+                        <MenuItem value={7} primaryText="7" />
+                        <MenuItem value={8} primaryText="8" />
+                        <MenuItem value={9} primaryText="9" />
+                        <MenuItem value={10} primaryText="10" />
+                        <MenuItem value={11} primaryText="11" />
+                        <MenuItem value={12} primaryText="12" />
                     </DropDownMenu>
 
-                    <br/>
+                    <br />
                     <section>
                         Total amount: {this.state.amount}
                     </section>
 
-                    <RaisedButton disabled={!this.state.formValid} onClick={this.handlePayClick} label="Pay Now"/>
-
+                    <RaisedButton onClick={this.handlePayClick.bind(this)} label="Pay Now" primary={true} buttonStyle={styles.buttonStyle} />
+                    <div><span>{this.state.response}</span></div>
                     <div><img style={styles.imgStyle} src="resources/cvv.jpg" alt="Cvv" /></div>
                 </form>
             </div>
