beca177f239bf63c3e6a51fda1497111a5509b9c
Vytautas Žilinas <vytautaszilinas@gmail.com>
2018-04-24 17:24:47 +0300

    BE: Fixed editing of discounts and some formating and a little swagger

--- RaisingTheBAR/RaisingTheBAR.BLL/Controllers/AdministratorController.cs
+++ RaisingTheBAR/RaisingTheBAR.BLL/Controllers/AdministratorController.cs
@@ -9,7 +9,7 @@ using System.Linq;
 
 namespace RaisingTheBAR.BLL.Controllers
 {
-    //[Authorize(Roles = "administrator")]
+    [Authorize(Roles = "administrator")]
     [Produces("application/json")]
     [Route("api/Administrator")]
     public class AdministratorController : Controller
@@ -21,7 +21,7 @@ namespace RaisingTheBAR.BLL.Controllers
         }
         [HttpPost]
         [ProducesResponseType(200)]
-        [ProducesResponseType(typeof(string),400)]
+        [ProducesResponseType(typeof(string), 400)]
         [ProducesResponseType(401)]
         [ProducesResponseType(403)]
         public IActionResult ChangeBlock([FromBody]BlockRequest request)
@@ -53,12 +53,12 @@ namespace RaisingTheBAR.BLL.Controllers
 
         [HttpGet("[action]")]
         [ProducesResponseType(typeof(IEnumerable<UserResponse>), 200)]
-        [ProducesResponseType(typeof(string),400)]
+        [ProducesResponseType(typeof(string), 400)]
         [ProducesResponseType(401)]
         [ProducesResponseType(403)]
         public IActionResult GetUsers()
         {
-            var userContext = _dbContext.Set<User>().Include(x=>x.Orders).ThenInclude(o=>o.ProductOrders).ThenInclude(po=>po.Product);
+            var userContext = _dbContext.Set<User>().Include(x => x.Orders).ThenInclude(o => o.ProductOrders).ThenInclude(po => po.Product);
 
             var userResponses = userContext.Select(x => new UserResponse
             {
@@ -80,7 +80,7 @@ namespace RaisingTheBAR.BLL.Controllers
 
         [HttpGet("[action]")]
         [ProducesResponseType(typeof(IEnumerable<FullProductResponse>), 200)]
-        [ProducesResponseType(typeof(string),400)]
+        [ProducesResponseType(typeof(string), 400)]
         [ProducesResponseType(401)]
         [ProducesResponseType(403)]
         public IActionResult GetProducts()
