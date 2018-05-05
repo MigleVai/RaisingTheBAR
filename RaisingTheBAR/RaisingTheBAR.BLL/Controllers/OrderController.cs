a30bfb04e2d45c4baf01540a20eb80cb934872ca
Unknown <vytautaszilinas@gmail.com>
2018-05-05 12:43:53 +0300

    BE: fixed images in orderlist

--- RaisingTheBAR/RaisingTheBAR.BLL/Controllers/OrderController.cs
+++ RaisingTheBAR/RaisingTheBAR.BLL/Controllers/OrderController.cs
@@ -192,7 +192,7 @@ namespace RaisingTheBAR.BLL.Controllers
                 {
                     Price = x.SinglePrice,
                     Id = x.Product.Id.ToString(),
-                    Image = x.Product.Images.FirstOrDefault(y => y.Type == ImageTypeEnum.Thumbnail).ImageBase64 ?? Configuration["DefaultThumbnail"],
+                    Image = x.Product.Images.FirstOrDefault(y => y.Type == ImageTypeEnum.Thumbnail) == null ? Configuration["DefaultThumbnail"] : x.Product.Images.FirstOrDefault(y => y.Type == ImageTypeEnum.Thumbnail).ImageBase64,
                     Name = x.Product.DisplayName,
                     Amount = x.Amount,
                     TotalPrice = x.SinglePrice * x.Amount
