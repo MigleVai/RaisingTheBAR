b0eac34f403f70ecf100f556505d430a7751715d
tautris <tautrimas.g2@gmail.com>
2018-05-11 21:37:18 +0300

    FE: added excel template download

--- RaisingTheBAR/RaisingTheBAR.React/client/src/components/Admin/Excel.jsx
+++ RaisingTheBAR/RaisingTheBAR.React/client/src/components/Admin/Excel.jsx
@@ -7,25 +7,40 @@ export default class Excel extends React.Component {
     super(props);
     this.state = {}
   }
-  handleExcelGenerate() {
-      axios({
-        url: '/api/Administrator/GenerateExcel',
-        method: 'GET',
-        responseType: 'blob', // important
-      }).then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'generated-excel.xlsx');
-        document.body.appendChild(link);
-        link.click();
-      });
+  handleExcelExport() {
+    axios({
+      url: '/api/Administrator/GenerateExcel',
+      method: 'GET',
+      responseType: 'blob', // important
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'generated-excel.xlsx');
+      document.body.appendChild(link);
+      link.click();
+    });
+  }
+  handleExcelTemplateDownload() {
+    axios({
+      url: '/api/Administrator/ProductImportTemplate',
+      method: 'GET',
+      responseType: 'blob', // important
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'excel-template.xlsx');
+      document.body.appendChild(link);
+      link.click();
+    });
   }
 
   render() {
     return (
       <div>
-        <FlatButton label="Generate excel" onClick={this.handleExcelGenerate} />
+        <FlatButton label="Export products" onClick={this.handleExcelExport} />
+        <FlatButton label="Download product import template" onClick={this.handleExcelTemplateDownload} />
       </div>
     )
   }
